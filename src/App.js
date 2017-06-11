331b0237d236240676871d635a0106ae4b58cf0c
Halil Bilir <hbilir@gmail.com>
2017-06-11 15:45:42 +0300

    streaming and displaying geojson data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,84 @@
 import React, { Component } from 'react';
+import JSONTree from 'react-json-tree';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connected: false,
+      node: null,
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    this.initNode();
+  }
+
+  initNode() {
+    const repoPath = 'ipfs-' + Math.random();
+    // Create an IPFS node
+    const node = new window.Ipfs({
+      init: false,
+      start: false,
+      repo: repoPath,
+    });
+    node.init(() => {
+      node.start(() => {
+        this.fetchData(node);
+        this.setState({
+          connected: true,
+          node,
+        });
+      });
+    });
+  }
+
+  fetchData(node) {
+    node.files.cat(
+      'QmZ7UVp6gb2Lqq8YGZpwhQP6oQYXejsRhFYroupK9pnVKe',
+      (err, stream) => {
+        let res = '';
+
+        stream.on('data', chunk => {
+          res += chunk.toString();
+        });
+
+        stream.on('error', err => {
+          console.error('Error - ipfs files cat ', err);
+        });
+
+        stream.on('end', () => {
+          this.setState({
+            data: JSON.parse(res),
+          });
+        });
+      }
+    );
+  }
+
   render() {
+    const { connected, data } = this.state;
+
+    let status;
+    if (connected) {
+      if (!data) {
+        status = 'Fetching Data';
+      }
+    } else {
+      status = 'Connecting';
+    }
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{status}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {data && <JSONTree data={data} />}
       </div>
     );
   }
